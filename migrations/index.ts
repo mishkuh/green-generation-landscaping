0836b00dd589d19b70ae5aa239c1670a4aec57c2
Mikey <mwiens@pm.me>
2026-01-23 11:12:39 -0800

    add media migration

--- migrations/index.ts
+++ migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260122_214229_initial from './20260122_214229_initial';
+import * as migration_20260123_185246_add_media_prefix from './20260123_185246_add_media_prefix';
 
 export const migrations = [
   {
     up: migration_20260122_214229_initial.up,
     down: migration_20260122_214229_initial.down,
-    name: '20260122_214229_initial'
+    name: '20260122_214229_initial',
+  },
+  {
+    up: migration_20260123_185246_add_media_prefix.up,
+    down: migration_20260123_185246_add_media_prefix.down,
+    name: '20260123_185246_add_media_prefix'
   },
 ];

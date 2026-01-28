9bea45830f5caf50c91368f34714db331d585e0e
Mikey <mwiens@pm.me>
2026-01-28 14:53:51 -0800

    create initial migration

--- migrations/index.ts
+++ migrations/index.ts
@@ -1,15 +1,9 @@
-import * as migration_20260122_214229_initial from './20260122_214229_initial';
-import * as migration_20260123_185246_add_media_prefix from './20260123_185246_add_media_prefix';
+import * as migration_20260128_222229_initial from './20260128_222229_initial';
 
 export const migrations = [
   {
-    up: migration_20260122_214229_initial.up,
-    down: migration_20260122_214229_initial.down,
-    name: '20260122_214229_initial',
-  },
-  {
-    up: migration_20260123_185246_add_media_prefix.up,
-    down: migration_20260123_185246_add_media_prefix.down,
-    name: '20260123_185246_add_media_prefix'
+    up: migration_20260128_222229_initial.up,
+    down: migration_20260128_222229_initial.down,
+    name: '20260128_222229_initial'
   },
 ];
